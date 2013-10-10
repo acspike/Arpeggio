0daa166dfe22c0d2009069915ae4750cd363ab9b
Aaron C. Spike <aaron@ekips.org>
2013-10-09 20:07:07 -0500

    add six scales

---
+++ example_6_reportlab.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from reportlab_fb import ReportlabFB, FBFlowable
+
+from arpeggio_arrays import *
+
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+WIDTH, HEIGHT = letter
+
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
+
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_LEFT
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+pdfmetrics.registerFont(TTFont('DejaVuSans', '/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf'))
+
+styles = getSampleStyleSheet()
+#print styles.list()
+styleN = styles['Normal']
+styleI = ParagraphStyle(name='',
+    parent=styles['Italic'],
+    alignment=2)
+styleT = ParagraphStyle(name='Title',
+    parent=styles['Title'], 
+    fontName='DejaVuSans', alignment=TA_LEFT)
+            
+
+
+fb7 = ReportlabFB(None, inch)
+fb7.u_height = 1.1
+fb7.u_width = 5.75
+fb7.num_frets = 20.0
+
+
+#mode Patterns
+from six_scales import scales
+
+
+doc = SimpleDocTemplate("six_scales.pdf",  pagesize=letter, bottomMargin = 0.25*inch, topMargin = 0.75*inch)
+story = []
+count = 0
+for title, notes in scales:
+    if title == '5th String 4th Finger':
+        story.append(PageBreak())   
+
+    story.append(Paragraph(title,styleT))
+    story.append(FBFlowable(fb7,notes))
+    
+    if not '1st' in title:
+        story.append(Spacer(1.5*inch, 1.5*inch)) 
+    
+doc.build(story)
